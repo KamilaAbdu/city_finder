5ec46eee7119a81adaf66b166fe766e4cfa7b5ec
Iuldash Abdurakhmanov <iuldash@MBP-Iuldash.lan>
2024-10-09 09:51:25 +0600

    реализация репозитория и api

---
+++ lib/domain/repository/city_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:city_finder/data/model/city.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CityRepository {
+  final Dio _dio = Dio();
+  final String _cacheKey = 'cached_cities';
+
+  Future<List<City>> getCities({String? search}) async {
+    try {
+      final response = await _dio.get(
+        'https://odigital.pro/locations/cities/',
+        queryParameters: {'search': search},
+      );
+
+      final cities =
+          (response.data as List).map((e) => City.fromJson(e)).toList();
+      await _cacheCities(cities);
+      return cities;
+    } catch (e) {
+      return await _getCachedCities() ?? [];
+    }
+  }
+
+  Future<void> _cacheCities(List<City> cities) async {
+    final prefs = await SharedPreferences.getInstance();
+    final cityJsonList =
+        cities.map((city) => jsonEncode(city.toJson())).toList();
+    prefs.setStringList(_cacheKey, cityJsonList);
+  }
+
+  Future<List<City>?> _getCachedCities() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cityJsonList = prefs.getStringList(_cacheKey);
+    if (cityJsonList != null) {
+      return cityJsonList
+          .map(
+              (city) => City.fromJson(jsonDecode(city) as Map<String, dynamic>))
+          .toList();
+    }
+    return null;
+  }
+}
